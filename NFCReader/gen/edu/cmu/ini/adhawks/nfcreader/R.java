29c81bec1c9d5e3eb96396791af4d3a92d4c7ea4
Gregory Owens <gowens@cmu.edu>
2012-03-15 01:21:22 -0400

    added reading methods

--- NFCReader/gen/edu/cmu/ini/adhawks/nfcreader/R.java
+++ NFCReader/gen/edu/cmu/ini/adhawks/nfcreader/R.java
@@ -14,10 +14,14 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
+        public static final int data=0x7f060004;
+        public static final int dataText=0x7f060005;
         public static final int discoveryText=0x7f060001;
         public static final int intentInfo=0x7f060003;
         public static final int tagDiscovery=0x7f060000;
         public static final int tagFromIntent=0x7f060002;
+        public static final int test=0x7f060006;
+        public static final int testText=0x7f060007;
     }
     public static final class layout {
         public static final int main=0x7f030000;
--- NFCReader/src/edu/cmu/ini/adhawks/nfcreader/NFCReaderActivity.java
+++ NFCReader/src/edu/cmu/ini/adhawks/nfcreader/NFCReaderActivity.java
@@ -1,5 +1,6 @@
 package edu.cmu.ini.adhawks.nfcreader;
 
+import java.io.IOException;
 import java.util.List;
 
 import edu.cmu.ini.adhawks.nfcreader.NdefMessageParser;
@@ -16,12 +17,15 @@ import android.nfc.tech.MifareClassic;
 import android.nfc.tech.MifareUltralight;
 import android.os.Bundle;
 import android.os.Parcelable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class NFCReaderActivity extends Activity
 {
+	TextView discoveryText, intentInfo, dataText, testText;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
@@ -30,13 +34,18 @@ public class NFCReaderActivity extends Activity
         setContentView(R.layout.main);
         
         //GUI setup
-        TextView discoveryText = (TextView) findViewById(R.id.discoveryText);
-        TextView intentInfo = (TextView) findViewById(R.id.intentInfo);
+        discoveryText = (TextView) findViewById(R.id.discoveryText);
+        intentInfo = (TextView) findViewById(R.id.intentInfo);
         
         //NFC stuff
         Intent intent = getIntent();
-        String action = intent.getAction();
-
+        resolveIntent(intent);
+    }
+    
+    public void resolveIntent(Intent intent)
+    {
+    	String action = intent.getAction();
+    	
         if(NfcAdapter.ACTION_NDEF_DISCOVERED.equals(action))
         {
             discoveryText.setText("NDEF Tag read!!");
@@ -44,21 +53,13 @@ public class NFCReaderActivity extends Activity
         // this is where we will be doing most of our work. Tech_Discovered is where all the other NFC formats are defined - GO
         else if(NfcAdapter.ACTION_TECH_DISCOVERED.equals(action))
         {
-        	discoveryText.setText("TECH Tag read!! /r/n Need to figure out what kind of tag it is");
-            
-            byte[] data;
-            Tag tagFromIntent = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
-            MifareClassic mfc = MifareClassic.get(tagFromIntent);
-            MifareUltralight mul = MifareUltralight.get(tagFromIntent); 
-            
-            intentInfo.setText(tagFromIntent.getTechList().toString());
-            
+        	discoveryText.setText("TECH Tag read!! Need to figure out what kind of tag it is");
+            Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
         }
         //if the intent is a Tag Discovered, process it
-        //TAG_DISCOVERED is a last resort action
-        else if(NfcAdapter.ACTION_TAG_DISCOVERED.equals(action))
+        else if(NfcAdapter.ACTION_TAG_DISCOVERED.equals(action)) //TAG_DISCOVERED is a last resort action
         {
-        	discoveryText.setText("TAG discovered!! /r/n This program really should be picking the tag up before this");
+        	discoveryText.setText("TAG discovered!!. This program really should be picking the tag up before this");
             
             //from here on is copied from NFCDemo -CD
             Parcelable[] rawMsgs = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
@@ -85,4 +86,83 @@ public class NFCReaderActivity extends Activity
         	discoveryText.setText("No tag read :(");
         }
     }
+    
+    //This is the code for reading MifareClassic cards, not sure if this actually works -GO    
+    public void readMiFareClassic(Tag tag)
+    {    
+    	  MifareClassic mfc = MifareClassic.get(tag);
+          
+          try 
+          {
+	            mfc.connect();
+	            boolean authenticate = false;
+	            byte[] byteData = null;
+	            String mfcData = null;
+	            
+	            //get the number of sectors the card has and loop through them
+	            int numSectors = mfc.getSectorCount();
+	            int numBlocks = 0;
+	            int blockIndex = 0;
+	            
+	            for(int sector = 0; sector < numSectors; sector++) 
+	            {
+	                //authenticate the sector
+	                authenticate = mfc.authenticateSectorWithKeyA(sector, MifareClassic.KEY_DEFAULT);
+	                
+	                if(authenticate)
+	                {
+	                    //get the block count in each sector
+	                	numBlocks = mfc.getBlockCountInSector(sector);
+	                	blockIndex = 0;
+	                    for(int block = 0; block < numBlocks; block++) 
+	                    {
+	                    	blockIndex = mfc.sectorToBlock(sector);
+	                        //read the block
+	                        byteData = mfc.readBlock(blockIndex);    
+	                        //Convert the data into a string from Hex format.                
+	                        //Log.i(TAG, getHexString(data, data.length));
+	                        blockIndex++;
+	                    }
+	                }
+	                else // Authentication failed - Handle it
+	                { 
+	                   
+	                }
+	            }
+          }
+          catch(IOException e) 
+          { 
+              //Log.e(TAG, e.getLocalizedMessage());
+              //showAlert(3);
+          }    	
+    }
+ 
+    //Still working on reading MiFareUltralight cards -GO
+    public void readMiFareUltralight(Tag tag)
+    {   
+        MifareUltralight mul = MifareUltralight.get(tag);     	
+    	
+        try 
+        {
+        	mul.connect();
+        	byte[] byteData = null;
+        	String mulData = null;
+        	int totalPages = 16;
+        	
+        	byteData = mul.readPages(0); 
+        	
+//        	for(int page = 0; page < totalPages; page++)
+//        	{
+//        		byteData = mul.readPages(page);
+//        		mulData += new String(byteData);
+//        	}
+        	
+//        	intentInfo.setText(mulData);
+        	intentInfo.setText(new String(byteData));
+        }
+        catch(Exception e) 
+        { 
+        	e.getLocalizedMessage();
+        }   	
+    }    
 }
