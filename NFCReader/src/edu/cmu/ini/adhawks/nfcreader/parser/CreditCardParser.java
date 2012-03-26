77db1ec81bcb135eff21c81d1a1a5f452828cecb
Gregory Owens <gowens@cmu.edu>
2012-03-25 20:03:22 -0400

    Initial Creditcard reading

--- NFCReader/src/edu/cmu/ini/adhawks/nfcreader/parser/CreditCardParser.java
+++ NFCReader/src/edu/cmu/ini/adhawks/nfcreader/parser/CreditCardParser.java
@@ -15,36 +15,11 @@ public class CreditCardParser {
 		// empty
 	}
 	
-	//Convert rec to binary for transceive to work
-	//Based on code from http://snippets.dzone.com/posts/show/93
-	public static byte[] intToByteArray(int value) {
-		byte[] b = new byte[4];
-		for (int i = 0; i < 4; i++)
-		{
-			int offset = (b.length - 1 - i) * 8;
-			b[i] = (byte) ((value >>> offset) & 0xFF);
-		}
-		return b;
-	}
-	
-	//Turns a byte array to string
-	public static String ByteArrayToString(byte[] byteArray)
-	{
-	  StringBuilder hex = new StringBuilder(byteArray.length*2);
-	  for(byte b : byteArray)
-	  {
-	    hex.append(String.format("{0:x2}",b));
-	  }
-	  return hex.toString();
-	}
-
-	
 	//This is the code for reading Credit Cards.  SK
 	//This currently does a SELECT statement where is grabs the card type. 
 	//Successfully reads the fact that the PNC card is Visa Debit 
 	public String readCreditCard(Tag tag)
 	{
-<<<<<<< HEAD
 		localIsoDep = IsoDep.get(tag);
 		try 
 		{
@@ -90,52 +65,6 @@ public class CreditCardParser {
 			//& is a bitwise AND operation 
 			int value = byteArray[i] & 0xff;
 			if (value < 16) 
-=======
-			IsoDep localIsoDep = IsoDep.get(tag);
-			NfcB localNfcB = NfcB.get(tag);
-			try 
-			{
-				localIsoDep.connect(); 													    // Connect to IsoDep
-				//byte[] cmd = {(byte)0x00,(byte)0xB2,(byte)0x01,(byte)0x0C,(byte)0x00};	// Command dealing with Visa PayWave from Stack Overflow
-				byte[] cmd = {(byte)0x00,(byte)0xA4,(byte)0x04,(byte)0x00,
-							  (byte)0x00,(byte)0xA0,(byte)0x00,(byte)0x00,
-							  (byte)0x00,(byte)0x03,(byte)0x10,(byte)0x10,
-							  (byte)0x00,(byte)0x90,(byte)0x00};							//byte array derived from OpenSCDP cmd
-				
-				//String sData = "0xBB"; 				// "got this magic value from web" - Stack overflow
-		        //byte []data = sData.getBytes();		//
-				
-				//new
-				for (int sfi = 1; sfi <= 31; sfi++)
-				{
-					for (int rec = 0x01; rec <= 0x10; rec++)
-					{
-						//convert ints sfi and rec into type Byte to be input into transceive						
-						byte[] recByte = intToByteArray(rec);
-						byte[] sfiByte = intToByteArray(sfi);
-						
-						//This code is modified from http://snippets.dzone.com/posts/show/93
-						byte[] tlv = localIsoDep.transceive((byte)0x00, (byte)0xB2, recByte, (sfi << (byte)0x03 | (byte)0x04, (byte)0x00); //(ByteArrayToString(sfiByte) << (byte)0x03) | (byte)0x04, (byte)0x00);
-						//card.sendApdu(0x00, 0xB2, rec, (sfi << 3) | 4, 0x00);
-						if (card.SW == 0x9000)//This is from the original 'card' format.  It needs to change to some sort of transceive output.  
-						{
-							
-						}
-					}
-				}
-				
-				
-				
-				//Old
-				byte[] response = localIsoDep.transceive(cmd);							// get response
-				
-				return new String(response);											//Return response as string
-				
-				
-				
-			}
-			catch(IOException e)
->>>>>>> 16e9d3f08659748ef07e55da147a2be225ae0fee
 			{
 				buffer.append('0');
 			}
@@ -145,7 +74,7 @@ public class CreditCardParser {
 		
 		return buffer.toString().toUpperCase();
 	}
-	
+
 	public String hexToString(String hex)
 	{	  
 		StringBuilder buffer = new StringBuilder();
@@ -197,5 +126,53 @@ public class CreditCardParser {
 		//byte[] unknownAID2 = {(byte)-96, (byte)0, (byte)0, (byte)0, (byte)3,(byte)16,(byte)16};
 		
 		return visaDebitAID;
-	}	
+	}
+	
+	//Stu is trying to working on putting something together here so don't touch. Thanks! - GO
+	public String stusReader(Tag tag)
+	{
+		IsoDep localIsoDep = IsoDep.get(tag);
+		NfcB localNfcB = NfcB.get(tag);
+		try 
+		{
+			localIsoDep.connect(); 													    // Connect to IsoDep
+			//byte[] cmd = {(byte)0x00,(byte)0xB2,(byte)0x01,(byte)0x0C,(byte)0x00};	// Command dealing with Visa PayWave from Stack Overflow
+			byte[] cmd = {(byte)0x00,(byte)0xA4,(byte)0x04,(byte)0x00,
+						  (byte)0x00,(byte)0xA0,(byte)0x00,(byte)0x00,
+						  (byte)0x00,(byte)0x03,(byte)0x10,(byte)0x10,
+						  (byte)0x00,(byte)0x90,(byte)0x00};							//byte array derived from OpenSCDP cmd
+			
+			//String sData = "0xBB"; 				// "got this magic value from web" - Stack overflow
+	        //byte []data = sData.getBytes();		//
+			
+			//new
+			for (int sfi = 1; sfi <= 31; sfi++)
+			{
+				for (int rec = 1; rec <= 16; rec++)
+				{
+					//convert ints sfi and rec into type Byte to be input into transceive						
+					byte recByte = (byte) rec;
+					byte sfiByte = (byte) sfi;
+					
+					//This code is modified from http://snippets.dzone.com/posts/show/93
+					//byte[] tlv = localIsoDep.transceive((byte)0x00, (byte)0xB2, recByte, (sfi << (byte)0x03 | (byte)0x04, (byte)0x00); //(ByteArrayToString(sfiByte) << (byte)0x03) | (byte)0x04, (byte)0x00);
+					//card.sendApdu(0x00, 0xB2, rec, (sfi << 3) | 4, 0x00);
+//					if (card.SW == 0x9000)//This is from the original 'card' format.  It needs to change to some sort of transceive output.  
+//					{
+//						
+//					}
+				}
+			}
+
+			//Old
+			byte[] response = localIsoDep.transceive(cmd);							// get response
+			
+			return new String(response);											//Return response as string	
+		}
+		catch(IOException e)
+		{
+			
+		}
+		return null;		
+	}
 }
